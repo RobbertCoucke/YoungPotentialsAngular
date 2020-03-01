f5f047ad44cf85239fcb1406d8162f308a45a35b
cherrymcgerry <robbert.coucke@hotmail.be>
2020-03-01 19:52:26 +0100

    added register functionality

--- src/app/_models/register.ts
+++ src/app/_models/register.ts
@@ -1,6 +1,12 @@
 
 export class Register {
 
+    constructor(email: string, password: string, isStudent: boolean){
+        this.email= email;
+        this.password = password;
+        this.isStudent = isStudent;
+    }
+
     //user
     email: string;
     password: string;
