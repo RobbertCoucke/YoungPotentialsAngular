d50750f644826f608c9ce6cb0450d8d100526b4d
Jarne Liebert <r0659473@student.vives.be>
2020-03-10 12:30:48 +0100

    solicitatie dialog

--- src/app/sollicitatie-dialog/sollicitatie-dialog.component.ts
+++ src/app/sollicitatie-dialog/sollicitatie-dialog.component.ts
@@ -5,7 +5,7 @@ import {
   AfterViewInit,
   ViewChild
 } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from "@angular/material";
 import {
   FormBuilder,
   FormGroup,
@@ -14,6 +14,7 @@ import {
 } from "@angular/forms";
 import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
 import { UploadComponent } from "@/upload/upload.component";
+import { DialogData } from "@/vacture-detail/vacture-detail.component";
 
 @Component({
   selector: "app-sollicitatie-dialog",
@@ -21,12 +22,16 @@ import { UploadComponent } from "@/upload/upload.component";
   styleUrls: ["./sollicitatie-dialog.component.scss"]
 })
 export class SollicitatieDialogComponent implements OnInit {
+  checkboxValue: boolean; // checkbox waarde voor algemene voorwaarden
 
-  checkboxValue: boolean;
+  uploadFile: FormData;
 
-  emailSender = "sender@gmail.com";
-  emailReciever = "reciever@gmail.com";
+  emailSender = this.data.UseremailValue; // email van de hudige gebruiker
+  emailReciever = this.data.companyEmailValue; // email van het bedrijf die vacature plaatste
 
+  /**
+   * * FormGroup per step in stepper
+   */
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
@@ -45,9 +50,11 @@ export class SollicitatieDialogComponent implements OnInit {
   onderwerpControl = new FormControl("", [Validators.required]);
   bodyControl = new FormControl("", [Validators.required]);
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  
+  constructor(
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<SollicitatieDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
   ngOnInit() {
     this.firstFormGroup = this.formBuilder.group({
@@ -61,12 +68,20 @@ export class SollicitatieDialogComponent implements OnInit {
     this.thirdFormGroup = this.formBuilder.group({});
     this.FourthFormGroup = this.formBuilder.group({});
     this.FifthFormGroup = this.formBuilder.group({
-    checkbox: this.checkbox
+      checkbox: this.checkbox
     });
   }
 
   changeCheck(event) {
     this.disabledAgreement = !event.checked;
+  }
+
+  handleUpload(formData: FormData) {
+    this.uploadFile = formData;
+  }
+
+  removeFile(){
+    this.uploadFile = null;
   }
 
   // checkFile(event) {
--- src/app/vacture-detail/vacture-detail.component.ts
+++ src/app/vacture-detail/vacture-detail.component.ts
@@ -1,10 +1,20 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import {
-  MatDialog,
-} from "@angular/material";
+import { Vacature } from './../_models/vacature';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import { SollicitatieDialogComponent } from "app/sollicitatie-dialog/sollicitatie-dialog.component";
 import { ActivatedRoute, Router } from '@angular/router';
 import { VacatureService } from '@/_services/Vacature/vacature.service';  
+import { AuthenticationService } from "@/_services/Authentication/authentication.service";
+import { User, Company } from '@/_models';
+import { UserService } from '@/_services/User/user.service';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+/**
+ * @description Interface om data door te geven naar dialog
+ */
+export interface DialogData {
+  UseremailValue: string;
+  companyEmailValue: string;
+}
 
 @Component({
   selector: 'app-vacture-detail',
@@ -17,30 +27,69 @@ export class VactureDetailComponent  {
   vacature : object;
   student : boolean;
 
+  userID : any;
+  currentUser: User;
+  UseremailValue: string;
+  cvURL: string;
+  user: User;
+  userHasCV: boolean;
+  userCV: string;
+
+  companyEmailValue: string;
   
   constructor(private dialog: MatDialog, private route: ActivatedRoute, 
-              private vacatureService: VacatureService) { 
+              private vacatureService: VacatureService, 
+              private authenticationService: AuthenticationService,
+              private userService: UserService, ) { 
                 this.route.paramMap.subscribe(params => {
                   this.id = parseInt(params.get('id'));
                   console.log(this.id);
                 });
-                this.vacatureService.getVacatureById(this.id).subscribe(data => this.vacature = data );
+                this.vacatureService.getVacatureById(this.id).subscribe(data => this.vacature = data);
+                this.vacatureService.getVacatureById(this.id).subscribe(data => this.companyEmailValue = data['email']);
               }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(SollicitatieDialogComponent, {
-      width: "1200px",
-      height: "500px",
-    });
-    console.log(this.$vacature);
-  }
 
  ngOnInit(): void {
    this.student == true;
+
+   this.authenticationService.currentUser.subscribe(u =>{
+    if(u.role === 'User'){
+        this.currentUser = u;
+        this.UseremailValue = u.email;
+        this.userService.getById(u.id).subscribe(c => {
+          this.user = c;
+          this.userCV = c.cvUrl;
+        });
+    }
+  });
+ }
+
+ 
+ openDialog(): void {
+  const dialogRef = this.dialog.open(SollicitatieDialogComponent, {
+    width: "1200px",
+    height: "500px",
+    data: {UseremailValue: this.UseremailValue,  companyEmailValue: this.companyEmailValue}
+  });
+  // console.log(this.$vacature);
+}
+
+ testgeg() {
+  console.log("Current user");
+  console.log(this.currentUser);
+  console.log(this.UseremailValue);
+  console.log("User");
+  console.log(this.user);
+  console.log("Company");
+  console.log(this.companyEmailValue);
+  console.log("CV");
+  console.log(this.userCV);
  }
 
  clicked(){
   console.log(this.vacature); 
+  console.log(this.vacature['email']);
  }
 
 }
