67691e3b43ff5f6805e4fa9391af106587889311
Wannes Vanneste <wannes.vanneste@student.vives.be>
2020-03-12 17:03:47 +0100

    merge fix

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -30,58 +30,6 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() { }
 
-<<<<<<< HEAD
-    console.log(this.registerForm.controls.value);
-
-    this.loading = true;  
-    //de register model krijgen   
-    var regModel = this.getRegisterModel();
-
-    console.log(regModel);
-
-
-    //de gebruiker toevoegen aan de database
-     this.authenticationService.register(regModel).subscribe(
-            data => {
-                this.router.navigate([""]);
-            },
-            error => {
-                this.error = error;
-                this.loading = false;
-            });   
-}
-
-  //get de register model nadat de gebruiker zijn data ingevuld heb
-  getRegisterModel(){
-    var controls = this.registerformControls;
-    //elke gebruiker moet zin email, password invullen
-    var model = new Register(controls.email.value, controls.password.value, this.isStudent);
-    model.telephone = controls.telephone.value;
-    //model.city = controls.city.value;
-    model.zipCode = controls.zipCode.value;
-    model.address = controls.address.value;
-    model.city = controls.city.value;
-
-    //de student moet zijn naam, voorname en CV geven.
-    if(this.isStudent){
-      model.name = controls.lastName.value;
-      model.firstName = controls.firstName.value;
-    }
-    //bedrijf moet een beschrijving, url en naam geven
-    else{
-
-      model.description = controls.description.value;
-      model.url = controls.url.value;
-      model.companyName = controls.companyName.value;
-    }
-
-    return model;
-
-  }
-
-  //register als student
-=======
->>>>>>> 6ab6e42a6f93ceb4245f65491a80866725116d2e
   studentClicked(){
     this.isStudent = true;
     console.log(this.isStudent);
