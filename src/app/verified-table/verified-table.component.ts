f4fb6ae2caf7490d53bb3cc065aab88e12306fae
Ibrahim Hag Kasem <ibrahem.hagkasem@student.vives.be>
2020-03-18 17:26:21 +0100

    role aanpassen

--- src/app/verified-table/verified-table.component.ts
+++ src/app/verified-table/verified-table.component.ts
@@ -60,7 +60,7 @@ export class VerifiedTableComponent {
     this.authenticationService.currentUser.subscribe(u => {
       this.currentUser = u; //ophalen huidig ingelogde gebruiker
       //als ingelogde gebruiker geen admin is worden ze geredirect naar de homepage
-      if (this.currentUser && this.currentUser.role !== Role.Admin) {
+      if ( !this.currentUser || this.currentUser.role !== Role.Admin) {
         this.router.navigate(["/"]);
       }
     });
